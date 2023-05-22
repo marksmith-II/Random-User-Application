1811c13c02907aa18a692c5f60ee7bdd16ac587f
Mark Smith <markssmith2@gmail.com>
2023-05-22 17:29:16 -0600

    User Detail Change - Buttons and code refactoring

--- lib/sceen/user_detail_page.dart
+++ lib/sceen/user_detail_page.dart
@@ -9,150 +9,190 @@ class UserDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile Information'),
-        backgroundColor: Colors.black54,
-        shadowColor: Colors.blue,
-      ),
-      body: Center(
-        child: Card(
-          color: Colors.blueGrey,
-          shadowColor: Colors.blueAccent,
-          child: Column(
-            children: [
-              const SizedBox(height: 140),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Image.network(user.picture.large),
-              ),
-              // const SizedBox(height: 20),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        user.fullName,
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.lightGreen,
-                        ),
-                      ),
-                      Text(
-                        "Gender: ${user.gender}",
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Age: ${user.dob.age}",
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Location: ${user.location.city}",
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-                width: 350,
-              ),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(50.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Contact Information:",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        "Phone: ${user.phone}",
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Cell: ${user.cell}",
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "Email: ${user.email}",
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      const Text(
-                        "Address:",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        user.location.fullStreetName,
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        "${user.location.city}, ${user.location.state}, ${user.location.postcode}",
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Text(
-                        user.location.country,
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-              // const SizedBox(height: 25),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text('Profile Information'),
+          backgroundColor: Colors.black54,
+          shadowColor: Colors.blue,
         ),
-      ),
-    );
+        body: Center(
+            child: Card(
+                color: const Color.fromARGB(255, 53, 74, 85),
+                shadowColor: Colors.blueAccent,
+                child: Column(children: [
+                  const SizedBox(height: 80),
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(70.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: Image.network(user.picture.large),
+                          ),
+                          Text(
+                            user.fullName,
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.lightGreen,
+                            ),
+                          ),
+                          Text(
+                            "Gender: ${user.gender}",
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            "Age: ${user.dob.age}",
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          Text(
+                            "Location: ${user.location.city}",
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                    width: 350,
+                  ),
+                  FloatingActionButton(
+                      backgroundColor: Colors.lightGreen,
+                      heroTag: 'Address',
+                      elevation: 10.0,
+                      child: const Icon(Icons.location_on),
+                      onPressed: () {
+                        // Show the address popup.
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text("Address"),
+                              content: Text.rich(
+                                TextSpan(children: [
+                                  TextSpan(
+                                      text: user.location.fullStreetName,
+                                      style: const TextStyle(
+                                          fontSize: 16.0, color: Colors.black)),
+                                  const TextSpan(
+                                      text: '\n',
+                                      style: TextStyle(
+                                          fontSize: 16.0, color: Colors.black)),
+                                  TextSpan(
+                                      text: user.location.city,
+                                      style: const TextStyle(
+                                          fontSize: 16.0, color: Colors.black)),
+                                  const TextSpan(
+                                      text: ' ',
+                                      style: TextStyle(
+                                          fontSize: 16.0, color: Colors.black)),
+                                  TextSpan(
+                                      text: user.location.state,
+                                      style: const TextStyle(
+                                          fontSize: 16.0, color: Colors.black)),
+                                  const TextSpan(
+                                      text: ', ',
+                                      style: TextStyle(
+                                          fontSize: 16.0, color: Colors.black)),
+                                  TextSpan(
+                                      text: user.location.postcode,
+                                      style: const TextStyle(
+                                          fontSize: 16.0, color: Colors.black)),
+                                  const TextSpan(
+                                      text: '\n',
+                                      style: TextStyle(
+                                          fontSize: 16.0, color: Colors.black)),
+                                  TextSpan(
+                                      text: user.location.country,
+                                      style: const TextStyle(
+                                          fontSize: 16.0, color: Colors.black))
+                                ]),
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text("Close"),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }),
+                  FloatingActionButton(
+                      backgroundColor: Colors.lightGreen,
+                      heroTag: 'Contact',
+                      elevation: 5.0,
+                      child: const Icon(Icons.contact_phone),
+                      onPressed: () {
+                        // Show the contact information popup.
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text("Contact Information"),
+                              content: Text.rich(
+                                TextSpan(
+                                    // text: '',
+                                    // style: const TextStyle(
+                                    //     fontSize: 16.0, color: Colors.black),
+                                    children: [
+                                      TextSpan(
+                                          text: "Phone: ${user.phone}",
+                                          style: const TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.black)),
+                                      const TextSpan(
+                                          text: '\n',
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.black)),
+                                      TextSpan(
+                                          text: "Cell:  ${user.cell}",
+                                          style: const TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.black)),
+                                      const TextSpan(
+                                          text: '\n',
+                                          style: TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.black)),
+                                      TextSpan(
+                                          text: "Email: ${user.email}",
+                                          style: const TextStyle(
+                                              fontSize: 16.0,
+                                              color: Colors.black)),
+                                    ]),
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text("Close"),
+                                ),
+                                // const SizedBox(height: 25),
+                              ],
+                            );
+                          },
+                        );
+                      })
+                ]))));
   }
 }
